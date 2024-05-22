effcdfa54d7557de39ae6bbeb12b179488e5c0dd
mnrx2020 <jamesmbere@gmail.com>
2024-05-22 19:21:37 +0300

    update change

--- index.js
+++ index.js
@@ -148,7 +148,6 @@ app.get("/allproducts", async (req, res) => {
 // Adding endpoint to fetch single product by ID
 app.get("/product/:id", async (req, res) => {
     const productId = parseInt(req.params.id, 10);
-    console.log("Product ID:", productId); // Log the product ID
     try {
         const product = await Product.findOne({ id: productId });
         if (!product) {
@@ -156,14 +155,14 @@ app.get("/product/:id", async (req, res) => {
         }
         const modifiedProduct = {
             ...product.toObject(),
-            image: getImageFilename(product.image)
+            image: `${process.env.BACKEND_URL}/images/${product.image}`
         };
         res.json(modifiedProduct);
-        console.log(modifiedProduct);
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
 
 // Schema creating for user model
 const Users = mongoose.model("Users", {
